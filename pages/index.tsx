57c1d9fd5b4c871884b9225f316cb757799c215a
Insaneninjan <beerjerkkingk@gmail.com>
2022-08-27 00:07:33 +0900

    feat: Set up Tailwind CSS
    PostCSSを利用し、Tailwindcssの導入・検証

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <p className='font-bold text-9xl'>hello</p>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
